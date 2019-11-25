e7eafa0b52057727cb1f0694f8dd5e2283d2b58f
xQsme <2151163@my.ipleiria.pt>
2019-11-25 18:58:35 +0000

    ªdone

--- src/utils/facades/LoginManagerApiFacade.js
+++ src/utils/facades/LoginManagerApiFacade.js
@@ -1,5 +1,5 @@
-//const URL_BASE = 'https://www.squirrel-mcif.me/api/';
-const URL_BASE = 'http://192.168.1.88/api/';
+const URL_BASE = 'https://www.squirrel-mcif.me/api/';
+//const URL_BASE = 'http://192.168.1.88:8000/api/';
 /**
  * The URL for the api method to dump the entire database
  * @type {string} - URL
